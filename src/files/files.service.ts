cf540e6316e5fc07523037549d95cb579c817e4a
LimitR <kakashihotakimail.ru@gmail.com>
2021-11-01 17:07:26 +0300

    Add product

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -9,7 +9,7 @@ export class FilesService {
     async createFileImg(file): Promise<string> {
         try {
             const fileName = uuid.v4() + '.jpg';
-            const filePath = path.resolve(__dirname, '..', 'static', 'img')
+            const filePath = path.resolve(__dirname, '..', 'static')
             if (!fs.existsSync(filePath)) {
                 fs.mkdirSync(filePath, {recursive: true})
             }
